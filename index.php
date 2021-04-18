1acf0b661a11f3e19b367806cd5501186bf7cd37
Fahmi Derbali <fahmiderbali@gmail.com>
2021-04-18 14:39:57 -0400

    Appeler le plugin jquery.form.js localement

--- index.php
+++ index.php
@@ -215,7 +215,7 @@
 
     <!--JavaScript at end of body for optimized loading-->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="http://malsup.github.com/jquery.form.js"></script>
+    <script src="js/jquery.form.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script src="js/paginpers.js"></script>
     <script src="js/app.js"></script>
